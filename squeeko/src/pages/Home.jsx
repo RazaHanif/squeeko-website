0e2a99cb11ea0b7b2f7359cbe89e8851dde61462
Raza Hanif <raza_hanif@ymail.com>
2025-09-19 15:48:09 -0400

    Working on React Refactor @ 3:48:08 PM

--- squeeko/src/pages/Home.jsx
+++ squeeko/src/pages/Home.jsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from "../components/theme-provider";
 import { ModeToggle } from "../components/mode-toggle";
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex-1">
-        <ModeToggle />
-      </div>
+    <>
+    <Button variant="default">Test</Button>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultricies posuere lorem eleifend venenatis. Aenean non leo in ex lobortis cursus. Sed odio libero, vehicula sed suscipit ut, ultrices fermentum ex. Cras consectetur volutpat metus, id porta ligula efficitur at. Aenean a mauris consequat, suscipit nibh a, commodo sem. In hac habitasse platea dictumst. Vestibulum sit amet scelerisque lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent bibendum in sapien nec viverra. Fusce ultrices pretium lacus, et sodales risus molestie quis.
       </p>
-    </ThemeProvider>
+    </>
   );
 }
 
