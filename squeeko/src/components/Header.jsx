fa128ea28b87d52d2c4d214ff16dfa965bda90bd
Raza Hanif <raza_hanif@ymail.com>
2025-09-19 13:07:48 -0400

    Working on React Refactor @ 1:07:47 PM

--- squeeko/src/components/Header.jsx
+++ squeeko/src/components/Header.jsx
@@ -2,28 +2,36 @@ import {
   NavigationMenu, NavigationMenuContent, NavigationMenuIndicator, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
     <div className="flex w-[100%] mx-0 my-auto border border-solid border-red-50">
         <div className="flex justify-between items-center font-roboto bg-primary py-2 px-4 my-4 mx-2 rounded-xs w-[100%]">
             <div className="text-4xl font-bold text-background">SQUEEKO</div>
-            <NavigationMenu>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                        <a href="/">
-                            HOME
-                        </a>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                        <a href="/about">
-                            ABOUT
-                        </a>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-            </NavigationMenu>
+            <ul className="list-none flex items-center justify-end m-0">
+                <li className="ml-5">
+                    <Link to={'/features'} className="text-background hover:underline">
+                        FEATURES
+                    </Link>
+                </li>
+                <li className="ml-5">
+                    <Link to={'/pricing'} className="text-background hover:underline">
+                        PRICING
+                    </Link>
+                </li>
+                <li className="ml-5">
+                    <Link to={'/about'} className="text-background hover:underline">
+                        ABOUT
+                    </Link>
+                </li>
+                <li className="ml-5">
+                    <Button variant="outline">
+                        Start Today
+                    </Button>
+                </li>
+            </ul>
+
         </div>
     </div>
   );
