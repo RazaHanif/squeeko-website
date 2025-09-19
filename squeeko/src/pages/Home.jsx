a28a6b90a6d9531ca4efee88eb618f1e2b3a8803
Raza Hanif <raza_hanif@ymail.com>
2025-09-19 15:55:25 -0400

    Working on React Refactor @ 3:55:25 PM

--- squeeko/src/pages/Home.jsx
+++ squeeko/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 const Home = () => {
   return (
     <>
-    <Button variant="default">Test</Button>
+    <Button variant="default" className="w-xs">Test</Button>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultricies posuere lorem eleifend venenatis. Aenean non leo in ex lobortis cursus. Sed odio libero, vehicula sed suscipit ut, ultrices fermentum ex. Cras consectetur volutpat metus, id porta ligula efficitur at. Aenean a mauris consequat, suscipit nibh a, commodo sem. In hac habitasse platea dictumst. Vestibulum sit amet scelerisque lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent bibendum in sapien nec viverra. Fusce ultrices pretium lacus, et sodales risus molestie quis.
       </p>
