23f587c8b3b7267952ec9399d83e7771748ebbc8
Raza Hanif <raza_hanif@ymail.com>
2025-09-19 19:09:31 -0400

    Working on React Refactor @ 7:09:31 PM

--- squeeko/src/components/Header.jsx
+++ squeeko/src/components/Header.jsx
@@ -4,31 +4,35 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className="flex w-[100%] mx-0 my-auto border border-solid border-red-50">
-        <div className="flex justify-between items-center font-roboto-mono bg-primary py-2 px-4 my-4 mx-2 rounded w-[100%]">
-            <div className="text-4xl font-bold text-background">SQUEEKO</div>
-            <ul className="list-none flex items-center justify-end m-0">
-                <li className="ml-5">
-                    <Link to={'/features'} className="text-background hover:underline">
-                        FEATURES
-                    </Link>
-                </li>
-                <li className="ml-5">
-                    <Link to={'/pricing'} className="text-background hover:underline">
-                        PRICING
-                    </Link>
-                </li>
-                <li className="ml-5">
-                    <Link to={'/about'} className="text-background hover:underline">
-                        ABOUT
-                    </Link>
-                </li>
-                <li className="ml-5">
-                    <Button variant="outline">
-                        Start Today
-                    </Button>
-                </li>
-            </ul>
+    <div className="header">
+        <div className="container">
+            <div className="logo">
+                SQUEEKO
+            </div>
+            <nav>
+                <ul className="nav-links">
+                    <li className="link-items">
+                        <Link to={'/features'} className="links">
+                            FEATURES
+                        </Link>
+                    </li>
+                    <li className="link-items">
+                        <Link to={'/pricing'} className="links">
+                            PRICING
+                        </Link>
+                    </li>
+                    <li className="link-items">
+                        <Link to={'/about'} className="links">
+                            ABOUT
+                        </Link>
+                    </li>
+                    <li className="link-items">
+                        <Button variant="outline">
+                            Start Today
+                        </Button>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
   );
