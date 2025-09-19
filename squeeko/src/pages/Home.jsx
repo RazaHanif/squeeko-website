6d6f3c9be289989867b8621610ffb960d4e1e91e
Raza Hanif <raza_hanif@ymail.com>
2025-09-19 10:50:35 -0400

    Working on React Refactor @ 10:50:35 AM

---
+++ squeeko/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+
+const Home = () => {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div>Home page Header</div>
+      <div>
+        <ModeToggle />
+      </div>
+      <div>Home Page Footer</div>
+    </ThemeProvider>
+  );
+}
+
+export default Home;
