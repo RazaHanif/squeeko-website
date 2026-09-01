624ea7637c34a27b67f811f9b93431e3bf261a21
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 15:26:30 -0400

    Q3 Build @ 3:26:30 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -39,7 +39,7 @@ function HorizontalScroll({ items=[], duration=20}) {
     }
 
     return (
-        <div className="w-full">
+        <div className="w-full border bprder">
             <div className="overflow-hidden relative">
                 <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-15 bg-gradient-to-r from-background to-transparent" />
                 <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-15 bg-gradient-to-l from-background to-transparent" />
