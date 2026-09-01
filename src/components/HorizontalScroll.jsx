65cef0d461dd05598477284283dee11d841cb392
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 12:06:42 -0400

    Q3 Build @ 12:06:42 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -18,7 +18,7 @@ function HorizontalScroll() {
 
             <div className="relative">
                 <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-30 bg-gradient-to-r from-background to-transparent" />
-                <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-24 bg-gradient-to-l from-background to-transparent" />
+                <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-30 bg-gradient-to-l from-background to-transparent" />
 
                 <div className="marquee marquee-8 gap-4">
                     {items.map((item) => (
