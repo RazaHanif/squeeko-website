fc708f87a36d8ba1eb4760ef63a060587c7ebcad
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 15:48:54 -0400

    Q3 Build @ 3:48:54 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -33,6 +33,8 @@ function HorizontalScroll({ items=[], speed=100}) {
         return () => observer.disconnect()
     }, [items])
 
+
+    
     let scrollList = []
     
     if (items.length > 0) {
