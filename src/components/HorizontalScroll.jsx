943b05b4fc17d1a786d72003e1346f3f5efda461
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 12:19:55 -0400

    Q3 Build @ 12:19:55 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -40,10 +40,6 @@ function HorizontalScroll({ items=[]}) {
 
     return (
         <div className="overflow-hidden border">
-            <p className="text-center text-sm text-muted-foreground">
-                Horizontal Scroll Testing
-            </p>
-
             <div className="relative">
                 <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-30 bg-gradient-to-r from-background to-transparent" />
                 <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-30 bg-gradient-to-l from-background to-transparent" />
