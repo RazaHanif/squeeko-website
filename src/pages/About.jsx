a3c23131772a94bcfa2b3b06e1b24a55e9727b52
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 17:20:35 -0400

    Q3 Build @ 5:20:35 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,23 +4,23 @@ function About() {
     return (
         <ScrollSnap className="flex flex-col flex-1 w-9/10 lg:w-3/4 lg:my-16 my-8">
 
-            <section className="snap-section min-h-[calc(100vh-80px)] border flex flex-col justify-center items-center w-full">
+            <section className="snap-section min-h-[calc(100vh-80px)] border flex flex-col justify-center items-center w-full border">
                 <h1 className="text-4xl lg:text-5xl font-serif">
                     About
                 </h1>
             </section>
 
-            <section className="snap-section min-h-[calc(100vh-80px)] border flex flex-col justify-center items-center w-full">
+            <section className="snap-section min-h-[calc(100vh-80px)] border flex flex-col justify-center items-center w-full border">
                 <p>
                     This should have some contact info, copy Jane.app mission, vision, and values format for a base. 
                 </p>
             </section>
 
-            <section className="snap-section min-h-[calc(100vh-80px)] border flex flex-col justify-center items-center w-full">
+            <section className="snap-section min-h-[calc(100vh-80px)] border flex flex-col justify-center items-center w-full border">
                 Features
             </section>
 
-            <section className="snap-section min-h-[calc(100vh-80px)] border flex flex-col justify-center items-center w-full">
+            <section className="snap-section min-h-[calc(100vh-80px)] border flex flex-col justify-center items-center w-full border">
                 CTA
             </section>
 
