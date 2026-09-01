c7f8fd98824a249060e1e45064644285f5d505ab
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 12:19:42 -0400

    Q3 Build @ 12:19:42 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -39,7 +39,7 @@ function HorizontalScroll({ items=[]}) {
     }
 
     return (
-        <div className="overflow-hidden py-12">
+        <div className="overflow-hidden py-12 border">
             <p className="text-center text-sm text-muted-foreground">
                 Horizontal Scroll Testing
             </p>
