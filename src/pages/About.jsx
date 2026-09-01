76ce3251801deaddce54e0ea0e924193ae344e93
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 17:35:31 -0400

    Q3 Build @ 5:35:31 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,8 +16,9 @@ function About() {
             </section>
 
             <section className="snap-section min-h-[calc(100vh-80px)] border-2 flex flex-col justify-center items-center w-full bg-muted text-muted-foreground">
-                
+                <p className="w-3/4">
                 Features
+                </p>
             </section>
 
             <section className="snap-section min-h-[calc(100vh-80px)] border-2 flex flex-col justify-center items-center w-full bg-muted text-muted-foreground">
