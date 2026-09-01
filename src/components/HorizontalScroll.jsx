280e405c0b2531a4560f0c2477f50f46948548ca
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 16:05:43 -0400

    Q3 Build @ 4:05:43 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -105,30 +105,3 @@ function HorizontalScroll({ items=[], speed=100, className}) {
 } 
 
 export default HorizontalScroll
-
-/* 
-        <div className="w-full border-2 border-red-500">
-            <div className="overflow-hidden relative">
-                <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-15 bg-gradient-to-r from-background to-transparent" />
-                <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-15 bg-gradient-to-l from-background to-transparent" />
-
-                <div className="marquee-track" style={{"--marquee-duration": `${duration}s`}}>
-                    <div className="marquee-group">
-                        {scrollList.map((item, idx) => (
-                            <div key={idx} className="marquee-item">
-                                {item}
-                            </div>
-                        ))}
-                    </div>
-
-                    <div className="marquee-group" aria-hidden="true">
-                        {scrollList.map((item, idx) => (
-                            <div key={idx} className="marquee-item">
-                                {item}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </div>
-*/
