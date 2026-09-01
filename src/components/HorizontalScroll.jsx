99c6b9e24cd532862723bc974534ef21c37be3dc
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 16:09:00 -0400

    Q3 Build @ 4:09:00 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -86,7 +86,7 @@ function HorizontalScroll({ items=[], speed=100, className}) {
                         <div
                             key={groupIndex}
                             ref={groupIndex === 0 ? groupRef : null}
-                            className="marquee-group gap-8"
+                            className="marquee-group gap-12"
                         >
                             {scrollList.map((item, idx) => (
                                 <div 
