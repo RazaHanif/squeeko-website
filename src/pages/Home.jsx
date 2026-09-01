b15a5b078468178c3d6eca8d7e3594ce92b864bb
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 16:54:58 -0400

    Q3 Build @ 4:54:58 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,23 +22,23 @@ function Home() {
                 </h1>
             </div>
 
-                <div className="flex flex-col justify-center items-center w-full text-muted-foreground">
-                    <p>
-                        This will be the landing page, should over view the offerings, and have atleast 2 'onboarding' forms
-                    </p>
-                    {(() => {
-                        const list = []
-                        for (let i = 0; i < 10; i++) {
-                            list.push(
-                                <p className="text-primary" key={i}>
-                                    {i}
-                                </p>
-                            )
-                        }
+            <div className="flex flex-col justify-center items-center w-full text-muted-foreground">
+                <p>
+                    This will be the landing page, should over view the offerings, and have atleast 2 'onboarding' forms
+                </p>
+                {(() => {
+                    const list = []
+                    for (let i = 0; i < 10; i++) {
+                        list.push(
+                            <p className="text-primary" key={i}>
+                                {i}
+                            </p>
+                        )
+                    }
 
-                        return list
-                    }) ()}
-                </div>
+                    return list
+                }) ()}
+            </div>
 
             <div className="mt-16">
                 <HorizontalScroll items={horizontalList} className="text-primary" speed={80}/>
