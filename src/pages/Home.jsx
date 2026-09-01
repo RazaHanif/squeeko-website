2f1dc884a21494f64062c2c4245ff681d0a7b5bb
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 12:30:20 -0400

    Q3 Build @ 12:30:20 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         'Shopaholic',
         'Voted Most Likely To Fall Asleep On The Beach',
         'Business Owner',
-        'Coming soon.',
+        'Boss',
     ]
     return (
         <div className="flex flex-col flex-1 w-9/10 lg:w-3/4 lg:py-16 py-8">
