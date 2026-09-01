15a526686b57f931c70e809c59ab8f7cdd25e6f2
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 16:03:14 -0400

    Q3 Build @ 4:03:14 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ function Home() {
             </div>
 
             <div className="my-16">
-                <HorizontalScroll className="bg-red-500"/>
+                <HorizontalScroll className="text-primary"/>
             </div>
 
             <div className="my-16">
