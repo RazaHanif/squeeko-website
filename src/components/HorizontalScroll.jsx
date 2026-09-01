2a4f1ed0552ff86b38da5cf2c2a1f544f4eccfcf
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 14:56:13 -0400

    Q3 Build @ 2:56:13 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -64,7 +64,7 @@ function HorizontalScroll({ items=[]}) {
                     ))}
                 </div> */}
 
-                <div className="marquee-track" style={{}}>
+                <div className="marquee-track" style={{"--marquee-duration": ``}}>
 
 
                 </div>
