6945116fb291c39387e7d61a9080d4f5995d9303
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 12:24:08 -0400

    Q3 Build @ 12:24:08 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -41,8 +41,8 @@ function HorizontalScroll({ items=[]}) {
     return (
         <div className="w-full">
             <div className="relative">
-                <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-30 bg-gradient-to-r from-background to-transparent" />
-                <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-30 bg-gradient-to-l from-background to-transparent" />
+                {/* <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-30 bg-gradient-to-r from-background to-transparent" />
+                <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-30 bg-gradient-to-l from-background to-transparent" /> */}
 
                 <div 
                     className="marquee"
