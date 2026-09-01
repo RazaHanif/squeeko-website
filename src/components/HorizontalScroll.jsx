4bcab95933bdc5227648251f5f61fb365806ec40
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 15:43:45 -0400

    Q3 Build @ 3:43:45 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -58,8 +58,8 @@ function HorizontalScroll({ items=[], duration=20}) {
                 ref={viewportRef}
                 className="overflow-hidden relative"
             >
-                <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-15 bg-gradient-to-r from-background to-primary" />
-                <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-15 bg-gradient-to-l from-background to-primary" />
+                <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-15 bg-gradient-to-r from-background to-transparent" />
+                <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-15 bg-gradient-to-l from-background to-transparent" />
 
                 <div 
                     className="marquee-track" 
