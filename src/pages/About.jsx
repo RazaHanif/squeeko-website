906f06fd97e55bc36e50d300a6c5d6fd9feb9d35
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 16:59:05 -0400

    Q3 Build @ 4:59:05 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,19 +17,19 @@ function About() {
 
             <ScrollSnap>
 
-                <section className="snap-section min-h- border bg-primary text-primary-foreground">
+                <section className="snap-section min-h-[calc(1)] border bg-primary text-primary-foreground">
                     Hero
                 </section>
 
-                <section className="snap-section min-h- border bg-secondary text-primary-foreground">
+                <section className="snap-section min-h-[calc(1)] border bg-secondary text-primary-foreground">
                     Testimonials
                 </section>
 
-                <section className="snap-section min-h- border bg-primary text-primary-foreground">
+                <section className="snap-section min-h-[calc(1)] border bg-primary text-primary-foreground">
                     Features
                 </section>
 
-                <section className="snap-section min-h- border bg-secondary text-primary-foreground">
+                <section className="snap-section min-h-[calc(1)] border bg-secondary text-primary-foreground">
                     CTA
                 </section>
 
