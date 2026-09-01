deee7fa7183a02821a008376f305dcffdd5a2d97
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 14:55:39 -0400

    Q3 Build @ 2:55:39 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -44,7 +44,7 @@ function HorizontalScroll({ items=[]}) {
                 <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-15 bg-gradient-to-r from-background to-transparent" />
                 <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-15 bg-gradient-to-l from-background to-transparent" />
 
-                <div 
+                {/* <div 
                     className="marquee"
                     style={{
                         "--marquee-items": scrollList.length,
@@ -62,7 +62,7 @@ function HorizontalScroll({ items=[]}) {
                             {item.toUpperCase()}
                         </div>
                     ))}
-                </div>
+                </div> */}
             </div>
         </div>
     )
