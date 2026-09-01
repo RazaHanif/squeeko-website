87551ee8114d1e4e5b79c5b15f890968c5916983
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 16:02:04 -0400

    Q3 Build @ 4:02:04 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react"
 
-function HorizontalScroll({ items=[], speed=100}) {
+function HorizontalScroll({ items=[], speed=100 clas}) {
     const groupRef = useRef(null)
     const viewportRef = useRef(null)
 
