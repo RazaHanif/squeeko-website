7806a35977143fc2f24d044b09fed60faffc3539
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 12:33:20 -0400

    Q3 Build @ 12:33:20 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
             </div>
 
             <div className="flex flex-col justify-center items-center text-muted-foreground my-16">
-                <p>Ghazal</p>
+                <p>GMS</p>
                 <HorizontalScroll items={horizontalList} />
             </div>
 
