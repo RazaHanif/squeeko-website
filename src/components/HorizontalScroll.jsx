acd645a74e7ffc8f592bd4275923a2f4a8060127
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 16:02:17 -0400

    Q3 Build @ 4:02:16 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -80,7 +80,7 @@ function HorizontalScroll({ items=[], speed=100, className}) {
                             className="marquee-group"
                         >
                             {scrollList.map((item, idx) => (
-                                <div key={idx} className="marquee-item">
+                                <div key={idx} className="marquee-item ">
                                     {item}
                                 </div>
                             ))}
