c3f519b4d43ce4639d521d740b25537dd3e40cb0
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 17:22:35 -0400

    Q3 Build @ 5:22:35 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,7 +10,7 @@ function About() {
             </section>
 
             <section className="snap-section min-h-[calc(100vh-80px)] border-2 flex flex-col justify-center items-center w-full bg-muted text-muted-foreground">
-                <p>
+                <p className="w-3/4">
                     This should have some contact info, copy Jane.app mission, vision, and values format for a base. 
                 </p>
             </section>
