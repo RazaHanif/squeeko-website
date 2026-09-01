f99b32ef5daa2b97bf1a76fff65697136b783b58
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 15:56:39 -0400

    Q3 Build @ 3:56:39 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -61,7 +61,7 @@ function HorizontalScroll({ items=[], speed=100}) {
         <div className="w-full">
             <div 
                 ref={viewportRef}
-                className="overflow-hidden relative bg-red-400 rounded-2xl"
+                className="overflow-hidden relative bg-red-400 rounded-xl"
             >
                 <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-15 bg-gradient-to-r from-background to-transparent" />
                 <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-15 bg-gradient-to-l from-background to-transparent" />
