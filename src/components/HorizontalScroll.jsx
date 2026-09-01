f3ae774d12616b86daacf374b02f9db6f3c90b1c
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 12:13:33 -0400

    Q3 Build @ 12:13:33 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -16,15 +16,13 @@ function HorizontalScroll() {
         "Test 13",
         "Test 14",
         "Test 15",
-        "Test 8",
-        "Test 1", 
-        "Test 2",
-        "Test 3",
-        "Test 4",
-        "Test 5",
-        "Test 6",
-        "Test 7",
-        "Test 8",
+        "Test 10",
+        "Test 11",
+        "Test 12",
+        "Test 13",
+        "Test 14",
+        "Test 15",
+
     ]
     return (
         <div className="overflow-hidden py-12">
