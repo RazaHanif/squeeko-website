c6b32bfd3203523ee7afd94e3ce2310a3e6b6997
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 16:43:52 -0400

    Q3 Build @ 4:43:52 PM

--- src/components/gsap/FadeUp.jsx
+++ src/components/gsap/FadeUp.jsx
@@ -1,3 +1,5 @@
+import { useGSAP } from "@gsap/react"
+
 gsap.registerPlugin(ScrollTrigger)
 
 function FadeUp({ 
@@ -7,6 +9,12 @@ function FadeUp({
     duration=0.8, 
     delay=0
 }) {
+
+    const container = useRef(null)
+
+    useGSAP(
+        
+    )
 
 }
 
