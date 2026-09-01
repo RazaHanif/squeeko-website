5a42ac78c348cef1d07124115b7ed7aba562fa42
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 17:06:59 -0400

    Q3 Build @ 5:06:59 PM

--- src/components/gsap/ScrollSnap.jsx
+++ src/components/gsap/ScrollSnap.jsx
@@ -9,7 +9,8 @@ function ScrollSnap({ children, className}) {
     const container = useRef(null)
 
     useGSAP(() => {
-        const sections = gsap.utils.toArray(".snap-section", container.current)
+        const sections = gsap.utils.toArray(
+            ".snap-section", container.current)
 
         ScrollTrigger.create({
             trigger: container.current,
