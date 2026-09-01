77d12c25aa5aad4704e3f8839c302f0b4acd7042
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 16:50:43 -0400

    Q3 Build @ 4:50:43 PM

--- src/components/gsap/ScrollSnap.jsx
+++ src/components/gsap/ScrollSnap.jsx
@@ -2,3 +2,9 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger)
+
+function ScrollSnap({ children, className}) {
+    
+}
+
+export default ScrollSnap
