5f4d88f709691096969723ee1757ea1afbeb3e73
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 15:38:33 -0400

    Q3 Build @ 3:38:32 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -55,6 +55,7 @@ function HorizontalScroll({ items=[], duration=20}) {
             ref={viewportRef}
             className="overflow-hidden"
         >
+            {/* Fade */}
             <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-15 bg-gradient-to-r from-background to-transparent" />
             <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-15 bg-gradient-to-l from-background to-transparent" />
 
