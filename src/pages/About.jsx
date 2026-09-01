fb2df1e91352d11eb9c0fab4049e978c9d8cd329
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 17:23:01 -0400

    Q3 Build @ 5:23:01 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import ScrollSnap from "@/components/gsap/ScrollSnap"
 
 function About() {
     return (
-        <ScrollSnap className="flex flex-col flex-1 w-9/10 lg:w-3/4 lg:my-16 my-8 gap-4">
+        <ScrollSnap className="flex flex-col flex-1 w-9/10 lg:w-3/4 lg:my-16 my-8 gap-4 font-mono">
             <section className="snap-section min-h-[calc(100vh-80px)] border-2 flex flex-col justify-center items-center w-full bg-muted text-muted-foreground">
                 <h1 className="text-4xl lg:text-5xl font-serif">
                     About
