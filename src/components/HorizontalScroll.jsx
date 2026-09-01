b3ebaaf8c8d13e3aadd9c39d49436c7bf71336b6
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 12:25:24 -0400

    Q3 Build @ 12:25:24 PM

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -45,7 +45,7 @@ function HorizontalScroll({ items=[]}) {
                 <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-30 bg-gradient-to-l from-background to-primary" />
 
                 <div 
-                    className="marquee"
+                    className="marquee bg-primary"
                     style={{
                         "--marquee-items": scrollList.length,
                         "--marquee-duration": `${scrollList.length * 2}s`
