3f56ae3d2768eec40161d3274c64b4a00d05d30c
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 17:01:52 -0400

    Q3 Build @ 5:01:52 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className="flex flex-col flex-1 w-9/10 lg:w-3/4 lg:py-16 py-8">
 
-            <ScrollSnap className={className="flex flex-col flex-1 w-9/10 lg:w-3/4 lg:py-16 py-8"}>
+            <ScrollSnap className="flex flex-col flex-1 w-9/10 lg:w-3/4 lg:py-16 py-8">
 
                 <section className="snap-section min-h-[calc(100vh-80px)] border bg-primary text-primary-foreground">
                     <div className="w-full flex justify-center items-center pb-8">
