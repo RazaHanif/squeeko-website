2a59840a32a4da62d019f18d9a5aa909d1e91b29
Raza Hanif <raza_hanif@ymail.com>
2026-09-01 16:07:52 -0400

    Q3 Build @ 4:07:52 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         'never miss another payment',
         'stay connected with families',
         'Built for childcare, by childcare experts',
-        'Business Owner',
+        'goodbye paperwork, ',
         'Boss',
     ]
 
