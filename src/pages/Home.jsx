36fb9b96d62e5b589b8ea5bc8bbc4fba8825ce59
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:27:06 -0400

    Q3 Build @ 11:27:06 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -83,16 +83,16 @@ function Home() {
             
             <section className="snap-section min-h-[calc(100vh-80px)] flex flex-col justify-start items-center gap-8 w-full bg-primary/50 text-primary-foreground">
                 <div className="p-8 flex flex-col justify-center items-center gap-2">
-                    <p className="text-center text-sm border">
+                    <p className="text-center text-sm border just">
                         FEATURES
                     </p>
-                    <h2 className="text-4xl font-serif text-center font-semibold border">
+                    <h2 className="text-4xl font-serif text-center font-semibold border just">
                         The tools your centre needs, all in one place
                     </h2>
                     <NavLink
                         to="/features"
                         end
-                        className="cursor-pointer hover:underline border" 
+                        className="cursor-pointer hover:underline border just" 
                     >
                         see all features &rarr;
                     </NavLink>
