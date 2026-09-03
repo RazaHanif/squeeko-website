25cdc02740df037b15e17c255f964e739866f4f7
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 12:23:55 -0400

    Q3 Build @ 12:23:55 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,15 +40,15 @@ function HomeFeatureAccordion() {
                         </p>
                     </div>
                     <div className="flex flex-col items-start border my-8">
-                        <NavLink to="#" end className="flex flex-row justify-center items-center">
+                        <NavLink to="#" end className="flex flex-row justify-center items-center text-lg">
                             <ChevronRight className="size-6"/>
                             Link
                         </NavLink>
-                        <NavLink to="#" end className="flex flex-row justify-center items-center">
+                        <NavLink to="#" end className="flex flex-row justify-center items-center text-lg">
                             <ChevronRight className="size-6"/>
                             Link
                         </NavLink>
-                        <NavLink to="#" end className="flex flex-row justify-center items-center">
+                        <NavLink to="#" end className="flex flex-row justify-center items-center text-lg">
                             <ChevronRight className="size-6"/>
                             Link
                         </NavLink>
