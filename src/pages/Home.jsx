77434743f6ca4591c5048219a8f03f52e3cd3235
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 12:38:34 -0400

    Q3 Build @ 12:38:34 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -358,7 +358,7 @@ function Home() {
                     </h2>
                 </div>
 
-                <div className="w-9/10 lg:w-3/4 border">
+                <div className="w-9/10 lg:w-3/4 border border-red-500">
                     <div className="text-center">
                         <NavLink
                             to="/features"
