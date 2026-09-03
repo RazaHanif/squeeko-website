3cdbfef25190a795eaf0207178b6c67f986e7baa
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 10:56:20 -0400

    Q3 Build @ 10:56:20 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,8 @@ function Home() {
                     CHILD CARE MANAGEMENT SOFTWARE  
                 </p>
                 <h1 className="text-4xl lg:text-5xl font-serif text-center">
-                    Your children have you,
+                    Your children have you,<br>
+                    </br>
                 </h1>
             </div>
                 <p className="w-3/4 text-center text-2xl">
