ddf927ba6c7bf2245259a88fd1443c11b4efbfee
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 12:26:36 -0400

    Q3 Build @ 12:26:36 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,7 +66,7 @@ function HomeFeatureAccordion() {
                         </NavLink>
                     </div>
 
-                    <div className="flex-1 flex flex-col justify-center items-center p-20 rounded-xl bg-red-500 border">
+                    <div className="flex-1 flex flex-col justify-center items-center p-20 rounded-xl bg-primary border">
                         <div className="">
                             Image
                         </div>
