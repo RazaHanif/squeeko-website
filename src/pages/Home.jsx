9347c59c6240aa515f4dd72a0a4b7d7c2cb29c8e
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 12:43:08 -0400

    Q3 Build @ 12:43:08 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -338,13 +338,13 @@ function Home() {
                 </div>
 
                 <div className="w-full flex flex-1 flex-col justify-center items-center border rounded-lg bg-primary text-primary-foreground">
-                    <div className="border ">
+                    <div className="border flex-1 ">
                         A
                     </div>
-                    <div className="border ">
+                    <div className="border flex-1 ">
                         B
                     </div>
-                    <div className="border ">
+                    <div className="border flex-1 ">
                         C
                     </div> 
                 </div>
