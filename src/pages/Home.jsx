15ec19ab91eac798e86a78f965e2f849bc509d21
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:32:11 -0400

    Q3 Build @ 11:32:11 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ import { NavLink } from "react-router-dom"
 
 function HomeFeatureAccordion() {
     return (
-        <Accordion className="">
+        <Accordion className="bg-red-500">
             <AccordionItem value="1">
                 <AccordionTrigger>
                     Scheduling
