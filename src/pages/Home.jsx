e7c04312b6344de41c00d4c05c838485e4f676fb
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 12:16:29 -0400

    Q3 Build @ 12:16:29 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,6 +40,12 @@ function HomeFeatureAccordion() {
                         <NavLink to="#" end>
                             Link
                         </NavLink>
+                        <NavLink to="#" end>
+                            Link
+                        </NavLink>
+                        <NavLink to="#" end>
+                            Link
+                        </NavLink>
                         <p className="!mb-0">links</p>
                         <p className="!mb-0 border">links</p>
                         <p className="!mb-0 border">links</p>
