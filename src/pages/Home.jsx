a84bd993224520af6620348c67f5a45e5b415f5c
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:19:10 -0400

    Q3 Build @ 11:19:10 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,7 +54,7 @@ function Home() {
                 </div>
             </section>
 
-            <div className="bg-red-500 w-full">
+            <div className="bg-red-500 w-9/10">
                 <HorizontalScroll items={horizontalList} className="text-primary" speed={80}/>
             </div>
             
