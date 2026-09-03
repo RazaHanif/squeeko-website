3a2a788bd713e23758ea1746d1ac7afa95c7a73e
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 13:02:23 -0400

    Q3 Build @ 1:02:23 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ function HomeFeatureAccordion() {
     const data = [
         {
             trigger: "Scheduling",
-            header: "Stay ",
+            header: "Stay on track",
             desc: "This will be a small description of what the scheduling feature does within the app, including attendance, ratio, and other helpful stuff.",
             links: [
                 {title: "Attendance", link: "#"},
