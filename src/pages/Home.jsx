6c33ac32d093f1d1b2f598a50654f6721a22df28
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 10:53:14 -0400

    Q3 Build @ 10:53:14 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,9 @@ function Home() {
         <div className="flex flex-col flex-1 w-9/10 lg:w-3/4 lg:py-16 py-8">
             <section className="snap-section min-h-[calc(100vh-80px)] flex flex-col justify-center items-center w-full border border-primary text-muted-foreground">
             <div className="w-full flex justify-center items-center pb-8">
+                <p>
+                    CHILDCARE 
+                </p>
                 <h1 className="text-4xl lg:text-5xl font-serif text-center">
                     Home
                 </h1>
