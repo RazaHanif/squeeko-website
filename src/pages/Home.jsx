750689879f835c638d51adc87c5f35cf218e936f
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:40:42 -0400

    Q3 Build @ 11:40:42 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ function HomeFeatureAccordion() {
     return (
         <Accordion
             defaultValue={["1"]}
-            className="w-full bg-primary rounded-2xl"
+            className="w-full"
         >
             <AccordionItem 
                 value="1"
