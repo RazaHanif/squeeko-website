5599c6bcaedaca3f61504490152aa5b7f643db9a
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:37:14 -0400

    Q3 Build @ 11:37:14 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ function HomeFeatureAccordion() {
                 value="1"
                 className="border-b"
             >
-                <AccordionTrigger>
+                <AccordionTrigger className="text-md">
                     Scheduling
                 </AccordionTrigger>
                 <AccordionContent className="flex flex-col">
