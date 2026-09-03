9030840746192093b096280760f978abbe07c1b4
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:54:51 -0400

    Q3 Build @ 11:54:51 AM

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -37,7 +37,7 @@ function AccordionTrigger({
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "group/accordion-trigger relative flex flex-1 items-start justify-between rounded-lg border border-red-500 p-2.5 text-left text-sm font-medium outline-none hover:underline focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 focus-visible:after:border-ring aria-disabled:pointer-events-none aria-disabled:opacity-50 **:data-[slot=accordion-trigger-icon]:ml-auto",
+          "group/accordion-trigger relative flex flex-1 items-start justify-between rounded-lg border border-transparent p-2.5 text-left text-sm font-medium outline-none hover:underline focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 focus-visible:after:border-ring aria-disabled:pointer-events-none aria-disabled:opacity-50 **:data-[slot=accordion-trigger-icon]:ml-auto",
           className
         )}
         {...props}>
