f0dc8bfbe4bf2856315f664756568421ce98ee80
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 10:56:36 -0400

    Q3 Build @ 10:56:36 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
                 </p>
                 <h1 className="text-4xl lg:text-5xl font-serif text-center">
                     Your children have you,<br/>
-                    your center has SQ
+                    your center has SQUEEKO
                 </h1>
             </div>
                 <p className="w-3/4 text-center text-2xl">
