565896de65e60a18f5f8191de4cf6a020f5807bf
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 13:02:54 -0400

    Q3 Build @ 1:02:54 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ function HomeFeatureAccordion() {
         {
             trigger: "Billing",
             header: "Get paid",
-            desc: "This will overview how you can collect payments within the app, including automating ",
+            desc: "This will overview how you can collect payments within the app, including automating late fees, field trips, ",
             links: [
                 {title: "Link 1", link: "#"},
                 {title: "Link 2", link: "#"},
