3f7d0c417ab4799fe05722f77c27006097d1d96c
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:47:42 -0400

    Q3 Build @ 11:47:41 AM

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -44,7 +44,7 @@ function AccordionTrigger({
         {children}
         <ChevronDownIcon
           data-slot="accordion-trigger-icon"
-          className="pointer-events-none shrink-0 group-aria-expanded/accordion-trigger:hidden" />
+          className="pointer-events-none shrink-0 group-aria-expanded/accordion-trigger:hidden fill-red-500" />
         <ChevronUpIcon
           data-slot="accordion-trigger-icon"
           className="pointer-events-none hidden shrink-0 group-aria-expanded/accordion-trigger:inline" />
