9a248260d8db8edf9dbae656069e4fa893bdd5ee
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 10:43:29 -0400

    Q3 Build @ 10:43:28 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
         <div className="flex flex-col flex-1 w-9/10 lg:w-3/4 lg:py-16 py-8">
             <section className="snap-section min-h-[calc(100vh-80px)] flex flex-col justify-center items-center w-full bg-gradient-to-r from-background to-primary text-muted-foreground">
                 <h1 className="text-4xl lg:text-5xl font-serif text-center">
-                    About
+                    Home Hero Section
                 </h1>
             </section>
 
