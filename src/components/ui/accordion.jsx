816e2cf350b20518b165e2e74332d19ae313d605
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:54:22 -0400

    Q3 Build @ 11:54:22 AM

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -42,16 +42,10 @@ function AccordionTrigger({
         )}
         {...props}>
         {children}
-        {/* <ChevronDownIcon
-          data-slot="accordion-trigger-icon"
-          className="pointer-events-none shrink-0 group-aria-expanded/accordion-trigger:hidden" /> */}
         <ChevronDownIcon
             data-slot="accordion-trigger-icon"
             className="pointer-events-none shrink-0 transition-transform duration-300 ease-in-out group-aria-expanded/accordion-trigger:rotate-180"
         />
-        {/* <ChevronUpIcon
-          data-slot="accordion-trigger-icon"
-          className="pointer-events-none hidden shrink-0 group-aria-expanded/accordion-trigger:inline" /> */}
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
   );
