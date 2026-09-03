44aa2ba8dffb27855d476a1d75d03481e99b0b5e
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 12:40:41 -0400

    Q3 Build @ 12:40:41 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -344,7 +344,7 @@ function Home() {
                 </div>
             </section>
 
-            <div className="w-full ">
+            <div className="w-full py-25">
                 <HorizontalScroll items={horizontalList} className="text-primary" speed={80}/>
             </div>
             
