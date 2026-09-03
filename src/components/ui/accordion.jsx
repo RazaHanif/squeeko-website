eb00597b59eea1c58d9ebf28e75a20efbbe506a0
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:49:01 -0400

    Q3 Build @ 11:49:01 AM

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -1,7 +1,7 @@
 import { Accordion as AccordionPrimitive } from "@base-ui/react/accordion"
 
 import { cn } from "@/lib/utils"
-import { ChevronDownIcon, ChevronUpIcon } from "lucide-react"
+import { ChevronDown, ChevronDownIcon, ChevronUpIcon } from "lucide-react"
 
 function Accordion({
   className,
@@ -42,7 +42,7 @@ function AccordionTrigger({
         )}
         {...props}>
         {children}
-        <ChevronDownIcon
+        <ChevronDown
           data-slot="accordion-trigger-icon"
           className="pointer-events-none shrink-0 group-aria-expanded/accordion-trigger:hidden text-red-500" />
         <ChevronUpIcon
