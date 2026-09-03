6efa0859898b76f32eca6b6870971d2eb844c12c
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 10:44:03 -0400

    Q3 Build @ 10:44:03 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ function Home() {
                     This will be the landing page, should over view the offerings, and have atleast 2 'onboarding' forms
                 </p>
                 <p className="w-3/4 text-center text-2xl">
-                    This will be the landing page, should over view the offerings, and have atleast 2 'onboarding' forms
+                    This should have a 
                 </p>
             </section>
 
