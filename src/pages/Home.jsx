240e046dabbee8ce80dc2c3b52440a52de3d46c7
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 13:04:54 -0400

    Q3 Build @ 1:04:54 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -51,7 +51,7 @@ function HomeFeatureAccordion() {
         {
             trigger: "Compliance",
             header: "Get in Compliance",
-            desc: "This will overview the amount of time saving ",
+            desc: "This will overview the amount of time saving and headach",
             links: [
                 {title: "Link 1", link: "#"},
                 {title: "Link 2", link: "#"},
