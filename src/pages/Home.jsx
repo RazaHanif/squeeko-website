df31b9a9f0d4ad2fc607d3f80b0a04743b3640ae
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:23:59 -0400

    Q3 Build @ 11:23:59 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -87,7 +87,7 @@ function Home() {
                 <p className="text-center text-xs">
                     FEATURES
                 </p>
-                <h2 className="">
+                <h2 className="text-5xl font-serif text-center font-semibold">
                     The tools your centre needs, all in one place
                 </h2>
 
