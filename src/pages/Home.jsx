43732c048eb4bd5f313354f99bc021e08b723bb8
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:22:10 -0400

    Q3 Build @ 11:22:10 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,8 @@ function HomeFeatureAccordion() {
     return (
         <Accordion defaultValue={["item-1"]}>
             <AccordionItem value="item-1">
-                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                <AccordionTrigger>
+                    Is it accessible?</AccordionTrigger>
                 <AccordionContent>
                     Yes. It adheres to the WAI-ARIA design pattern.
                 </AccordionContent>
