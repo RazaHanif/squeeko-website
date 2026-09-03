a6b911a426f2f062f0c5f6d4985090a92bc7b021
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 12:18:07 -0400

    Q3 Build @ 12:18:07 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,7 +38,7 @@ function HomeFeatureAccordion() {
                         Description
                     </p>
                     <div className="flex flex-col items-start gap-2">
-                        <NavLink to="#" end className="flex flex-row justify-center items-center">
+                        <NavLink to="#" end className="flex flex-row justify-center items-center hover:underline">
                             <ChevronRight className="size-6"/>
                             Link
                         </NavLink>
