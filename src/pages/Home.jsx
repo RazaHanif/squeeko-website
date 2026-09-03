72f493391d35b99718a5a127b1561e8174fd7d26
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 12:25:53 -0400

    Q3 Build @ 12:25:53 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -67,7 +67,7 @@ function HomeFeatureAccordion() {
                     </div>
 
                     <div className="flex-1 flex flex-col justify-center items-center p-20 border">
-                        <div className="border p-4">
+                        <div className="border p-4 rounded-xl">
                             Image
                         </div>
                     </div>
