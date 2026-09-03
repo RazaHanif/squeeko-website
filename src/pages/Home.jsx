34140784cae3e84bd27be1c2a4d751de4c4a90e5
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 12:53:59 -0400

    Q3 Build @ 12:53:58 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -78,7 +78,7 @@ function HomeFeatureAccordion() {
             defaultValue={[data[0].trigger]}
             className="w-full rounded-lg bg-primary p-6"
         >
-            {data.map(({trigger, }) => (
+            {data.map(({trigger, header, desc, links, image}) => (
                 <AccordionItem
                     key={category}
                     value={category}
