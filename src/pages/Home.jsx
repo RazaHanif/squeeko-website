91cc1a969c32bb0dcae4f3789c61f5a120b89af7
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 13:01:10 -0400

    Q3 Build @ 1:01:09 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ function HomeFeatureAccordion() {
             links: [
                 {title: "Attendance", link: "#"},
                 {title: "Ratio", link: "#"},
-                {title: "Sick Day", link: "#"},
+                {title: "Late Fees", link: "#"},
             ],
             image: "IMAGE"
         },
