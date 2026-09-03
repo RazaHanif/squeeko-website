ea67de8dee90243a36ea35f449d138e760cbfd1a
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:42:15 -0400

    Q3 Build @ 11:42:15 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -122,7 +122,7 @@ function Home() {
                     </NavLink>
                 </div>
 
-                <div className="w-9/10 lg:w-3/4 bg-primary">
+                <div className="w-9/10 lg:w-3/4 bg-primary rounded-2xl">
                     <HomeFeatureAccordion />
                 </div>
 
