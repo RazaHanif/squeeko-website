23d964661bee045b1087e8738b2c63f413c444c0
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:58:08 -0400

    Q3 Build @ 11:58:08 AM

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -59,7 +59,7 @@ function AccordionContent({
   return (
     <AccordionPrimitive.Panel
       data-slot="accordion-content"
-      className="overflow-hidden text-sm data-open:animate-accordion-down data-closed:animate-accordion-up border p-4"
+      className="overflow-hidden text-sm data-open:animate-accordion-down data-closed:animate-accordion-up border"
       {...props}>
       <div
         className={cn(
