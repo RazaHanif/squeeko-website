509d7b32aa04a444f79857c5ce5086f06be3d3b9
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:21:50 -0400

    Q3 Build @ 11:21:50 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,16 @@ import {
 
 
 function HomeFeatureAccordion() {
-    
+    return (
+        <Accordion defaultValue={["item-1"]}>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+    )
 }
 
 function Home() {
