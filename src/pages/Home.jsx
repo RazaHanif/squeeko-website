5da34e7a3460adb61d79079d7bde89d196b9efbb
Raza Hanif <raza_hanif@ymail.com>
2026-09-03 11:02:51 -0400

    Q3 Build @ 11:02:51 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
         <div className="flex flex-col flex-1 w-9/10 lg:w-3/4 lg:py-16 py-8">
             <section 
                 className="snap-section min-h-[calc(100vh-80px)] w-full flex flex-col justify-start items-center">
-                <div className="w-full flex flex-col justify-center items-center p-8">
+                <div className="w-full flex flex-col justify-center items-center p-8 gap-4">
                     <p className="text-xs font-bold">
                         CHILD CARE MANAGEMENT SOFTWARE  
                     </p>
